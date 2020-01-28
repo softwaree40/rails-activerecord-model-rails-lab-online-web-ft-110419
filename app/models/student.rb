5b440afbafdd29b8f012b2d320e9834fe77cc15f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-28 19:10:25 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,8 @@
 class Student < ActiveRecord::Base
-<<<<<<< HEAD
+
      def to_s
-=======
-     def full_name
->>>>>>> eb0d4d940f8fc25af881e6102f403d9c0931ed98
-       
-        "#{self.first_name}" - "#{self.last_name}" 
+
+     "#{self.first_name}" - "#{self.last_name}" 
        
      end
   
